1b3b5bbaff4c159f2b62da5fdc981d68bc547e92
wujunlong <yearthmain@gmail.com>
2020-11-20 16:48:08 +0800

    [feature]: 添加 git 和 zsh 配置
    git：template
    zsh：alias

---
+++ zsh/alias.zsh
@@ -0,0 +1,43 @@
+# git
+alias ga="git add"
+alias gc="git commit"
+alias gs="git status"
+alias gph="git push"
+alias gpl="git pull"
+alias gch="git checkout"
+alias gb="git branch"
+alias gbl="git branch --list"
+alias gcf="git config --global commit.template /home/yearth/.git-config/feature-template && git commit"
+alias gcr="git config --global commit.template /home/yearth/.git-config/refactor-template && git commit"
+alias gcb="git config --global commit.template /home/yearth/.git-config/bugfix-template && git commit"
+
+# npm
+alias ni="npm install"
+alias nd="npm run dev"
+alias ns="npm run serve"
+alias nb="npm run build"
+
+# zsh
+alias sz="source /home/yearth/.oh-my-zsh/custom/custom.zsh"
+
+# npm scripts
+alias ns="npm run serve"
+alias nd="npm run dev"
+alias nt="npm run test"
+alias ntu="npm run test:unit"
+
+# yarn
+alias ys="yarn start"
+alias ya="yarn add"
+
+# hexo
+alias hs="hexo server"
+alias hg="hexo g -d"
+alias hc="hexo clean"
+
+# node
+alias n="node"
+alias nm="nodemon"
+
+# windows
+alias open=cmd.exe /C start
